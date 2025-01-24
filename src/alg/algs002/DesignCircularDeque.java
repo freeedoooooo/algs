e4d5844d1ab4b2b038303f30707bada1160cb951
shadow <hooray2010@foxmail.com>
2025-01-24 14:37:39 +0800

    add circular deque

---
+++ src/alg/algs002/DesignCircularDeque.java
@@ -0,0 +1,101 @@
+package alg.algs002;
+
+// 设计循环双端队列
+// 测试链接 : https://leetcode.cn/problems/design-circular-deque/
+public class DesignCircularDeque {
+
+    public int[] queue;
+    public int head;
+    public int tail;
+    public int size;
+    public int limit;
+
+    public DesignCircularDeque(int n) {
+        queue = new int[n];
+        head = 0;
+        tail = 0;
+        size = 0;
+        limit = n;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) {
+            return false;
+        } else {
+            if (isEmpty()) {
+                queue[0] = value;
+                // 【！！！注意此处要重置 head & tail 指针的位置】
+                head = tail = 0;
+            } else {
+                head = (head == 0) ? limit - 1 : head - 1;
+                queue[head] = value;
+            }
+            size++;
+            return true;
+        }
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) {
+            return false;
+        } else {
+            if (isEmpty()) {
+                queue[0] = value;
+                // 【！！！注意此处要重置 head & tail 指针的位置】
+                head = tail = 0;
+            } else {
+                tail = (tail + 1 == limit) ? 0 : tail + 1;
+                queue[tail] = value;
+            }
+            size++;
+            return true;
+        }
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) {
+            return false;
+        } else {
+            queue[head] = 0;
+            head = (head + 1 == limit) ? 0 : head + 1;
+            size--;
+            return true;
+        }
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) {
+            return false;
+        } else {
+            queue[tail] = 0;
+            tail = (tail == 0) ? limit - 1 : tail - 1;
+            size--;
+            return true;
+        }
+    }
+
+    public int getFront() {
+        if (isEmpty()) {
+            return -1;
+        } else {
+            return queue[head];
+        }
+    }
+
+    public int getRear() {
+        if (isEmpty()) {
+            return -1;
+        } else {
+            return queue[tail];
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == limit;
+    }
+
+}
---
+++ src/alg/algs002/MinStack.java
@@ -0,0 +1,38 @@
+package alg.algs002;
+
+import java.util.Stack;
+
+// 最小栈
+// 测试链接 : https://leetcode.cn/problems/min-stack/
+public class MinStack {
+
+    private Stack<Integer> dataStack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        dataStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
