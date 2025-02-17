fbd95187f0c9ebfe94444cde3ccec8214c545553
shadow <hooray2010@foxmail.com>
2025-02-17 15:11:07 +0800

    review BinaryTreePreorderTraversal

--- src/leetcode/p144_binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
+++ src/leetcode/p144_binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-// 用一个栈完成先序遍历
-// 测试链接 : https://leetcode.cn/problems/binary-tree-preorder-traversal/
+/**
+ * 用一个栈完成先序遍历
+ * 测试链接 : https://leetcode.cn/problems/binary-tree-preorder-traversal
+ *
+ * @author Administrator
+ */
 public class BinaryTreePreorderTraversal {
 
     public static class TreeNode {
@@ -18,25 +22,38 @@ public class BinaryTreePreorderTraversal {
         }
     }
 
+    /**
+     * 先序遍历
+     * 先压栈右孩子，后压栈左孩子；
+     * 左孩子先出栈，再循环上一步。
+     */
     public static List<Integer> preorderTraversal(TreeNode head) {
+        // 收集打印的结果
         List<Integer> result = new ArrayList<>();
-        if (head != null) {
-            Stack<TreeNode> stack = new Stack<>();
-            stack.push(head);
-            while (!stack.isEmpty()) {
-                // 每当弹出一个节点，就打印这个节点
-                head = stack.pop();
-                result.add(head.val);
-                // 【注意！！！】先压栈右孩子
-                if (head.right != null) {
-                    stack.push(head.right);
-                }
-                // 后压栈左孩子
-                if (head.left != null) {
-                    stack.push(head.left);
-                }
+        if (head == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        // 先压栈头节点
+        stack.push(head);
+
+        while (!stack.isEmpty()) {
+            // 每当弹出一个节点，就打印这个节点
+            head = stack.pop();
+            result.add(head.val);
+
+            // 【注意！！！】先压栈右孩子，后出栈
+            if (head.right != null) {
+                stack.push(head.right);
+            }
+
+            // 后压栈左孩子，所以会先出栈
+            if (head.left != null) {
+                stack.push(head.left);
             }
         }
+
         return result;
     }
 
