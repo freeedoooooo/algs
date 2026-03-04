f1d3809b94ee481b5f083f67ebd400dfced168ce
shadow <hooray2010@foxmail.com>
2026-03-04 10:17:36 +0800

    review leetcode

--- src/leetcode/p086_partition_list/PartitionList.java
+++ src/leetcode/p086_partition_list/PartitionList.java
@@ -3,10 +3,13 @@ package leetcode.p086_partition_list;
 import util.collection.ListNode;
 
 /**
- * 测试链接 : <a href="https://leetcode.cn/problems/partition-list/">86.分割链表</a>
+ * 分割链表
+ * 测试链接 : https://leetcode.cn/problems/partition-list
  * 给你一个链表的头节点 head 和一个特定值 x
  * 请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
  * 你应当 保留 两个分区中每个节点的初始相对位置
+ *
+ * @author hurui
  */
 public class PartitionList {
 
